3da425df1fcd9452c761c276dd285e5b7774db29
Shaun <sme170330@utdallas.edu>
2020-08-30 23:18:15 -0500

    Project Initialization

---
+++ Assets/Scripts/GameInput.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameInput : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Backspace))
+        {
+            ReloadLevel();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
+    
+
+    void ReloadLevel()
+    {
+        int activeSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(activeSceneIndex);
+    }
+}
---
+++ Assets/Scripts/PlayerShip.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerShip : MonoBehaviour
+{
+    [SerializeField] float _moveSpeed = 12f;
+    [SerializeField] float _turnSpeed = 3f;
+    public ParticleSystem thrusters;
+    public bool includeChildren = true;
+   
+
+    Rigidbody _rb = null;
+
+    private void Update()
+    {
+
+        if (!Input.GetButton("Vertical"))
+        {
+            thrusters.Play(includeChildren);
+        }
+    }
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        MoveShip();
+        TurnShip();
+    }
+    
+    //use force to build moment forward/backward
+    void MoveShip()
+    {
+        // s/down =-1, w/up =1, none=0. scale by movespeed
+        float moveAmountThisFrame = Input.GetAxisRaw("Vertical") * _moveSpeed;
+        //combine direction with calculated amount
+        Vector3 moveDirection = transform.forward * moveAmountThisFrame;
+        //apply the movement to the physics object
+        _rb.AddForce(moveDirection);
+
+    }
+    void TurnShip()
+    {
+        // a/left= -1, d/right = 1, none= 0. scaled by turnspeed
+        float turnAmountThisFrame = Input.GetAxisRaw("Horizontal") * _turnSpeed;
+        //specify an axis to apply our turn amount as a rotation
+        Quaternion turnOffset = Quaternion.Euler(0, turnAmountThisFrame, 0);
+        //spin rigidbody
+        _rb.MoveRotation(_rb.rotation * turnOffset);
+    }
+    public void Kill()
+    {
+        Debug.Log("Player has been killed!");
+        this.gameObject.SetActive(false);
+    }
+    
+}
